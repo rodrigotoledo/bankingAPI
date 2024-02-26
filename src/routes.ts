86af10be9859cc5628aa762b4dfa0be2f688dc0c
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-02-26 18:54:11 -0300

    fix: get route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import { FastifyInstance,  FastifyPluginOptions, FastifyRequest, FastifyReply    }from "fastify"
 import { createCustomerController } from "./controllers/createCustomerController"
 import { deleteCustomerController } from "./controllers/deleteCustomerController"
+import { listCustomerController } from "./controllers/listCustomersController"
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
-    fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) =>{
-        return{ok: true }
-    })
+    // fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) =>{
+    //     return{ok: true }
+    // }) Rota de teste
 
     fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) =>{
         return new createCustomerController().handle(request, reply)
@@ -13,5 +14,9 @@ export async function routes(fastify: FastifyInstance, options: FastifyPluginOpt
 
     fastify.delete("/customer", async (request: FastifyRequest, reply: FastifyReply) =>{
         return new deleteCustomerController().handle(request, reply)
+    })
+
+    fastify.get("/customers", async (request: FastifyRequest, reply: FastifyReply) =>{
+        return new listCustomerController().handle(request, reply)
     })
 }
