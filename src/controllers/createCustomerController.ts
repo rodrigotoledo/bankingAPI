2c42aaa3071ab4b170d61862606bd096466dee7c
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-02-27 00:23:36 -0300

    feat: Costumer balance added

--- src/controllers/createCustomerController.ts
+++ src/controllers/createCustomerController.ts
@@ -4,11 +4,11 @@ import {createCustomerService} from "../services/createCustomerService"
 class createCustomerController{
     async handle(request:FastifyRequest, reply:FastifyReply){
 
-        const {name, email} = request.body as {name: string, email: string};
+        const {name, email, balance} = request.body as {name: string, email: string, balance: number};
 
         const customerService = new createCustomerService()
 
-        const customer = await customerService.execute({name, email});   
+        const customer = await customerService.execute({name, email, balance});   
 
         reply.send(customer)
     }
